9bb4ab2fcd57840eb3843b47ca9a4cee98c9894c
shasteroc <shasteroca@gmail.com>
2024-10-25 17:31:50 -0500

    Decimo commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+export const LandingPage = () => {
+  return (
+    <body>
+        <h1>Bienvenido a <em>Travel</em> <br />el Blog de tus Destinos Turisticos</h1>
+        <img src="src\assets\Logo.png" alt="" />  
+        <p className="p">Aqui podras calificar el destino turistico en el que estuviste</p>
+    </body>
+  )
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { Login } from "../components/Login";
 import { Results } from "../components/Results";
 import { Registro } from "../components/Registro";
+import { LandingPage } from "../components/LandingPage";
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    Component: LandingPage
   },
   {
     path: '/Registro',
